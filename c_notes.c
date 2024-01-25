39145a7a17c3941cef5ff91a86cc1cbd18de41a2
L Sponza <61894715+SponzaPlusPlus@users.noreply.github.com>
2024-01-25 13:03:46 +1100

    Update c_notes.c

--- c_notes.c
+++ c_notes.c
@@ -1,6 +1,17 @@
 // C Notes
 
+int main()
+{
 /*
 	register keyword ... CPU
+
+	https://www.skillvertex.com/blog/understanding-register-keyword-in-c/#:~:text=The%20register%20keyword%20can%20only,in%20which%20they%20are%20defined. :
+ 
+ 	"The register keyword can only be used within a block or function scope (local scope)
+  	and cannot be used in the global scope (outside of any function). In C, variables declared
+   	with the register keyword are local to the block or function in which they are defined."
 */
-register int var_aaa = 0;
+	register int var_aaa = 0;
+
+	return 0;
+)
